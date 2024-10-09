f6d72b5a18219e5d5b672cab6e53e9be4e02a6c4
bidingurusmk <bidingurusmk@gmail.com>
2024-10-09 10:48:44 +0700

    rev

--- lib/views/movie_view.dart
+++ lib/views/movie_view.dart
@@ -1,26 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:navigasi_flutter/controllers/movie_controller.dart';
+import 'package:navigasi_flutter/widgets/modal.dart';
 
-class MovieView extends StatelessWidget {
+class MovieView extends StatefulWidget {
   const MovieView({super.key});
 
+  @override
+  State<MovieView> createState() => _MovieViewState();
+}
+
+class _MovieViewState extends State<MovieView> {
+  MovieController movie = MovieController();
+  List buttonChoice = ["Updaate", "Hapus"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Movie"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                ModalWidget().showFullModal(context, addItem());
+              },
+              icon: Icon(Icons.add))
+        ],
       ),
       body: ListView.builder(
-          itemCount: 4,
+          itemCount: movie.movie.length,
           itemBuilder: (context, index) {
             return Card(
-              child: Row(
-                children: [
-                  Text("data"),
-                  Text("data"),
-                ],
+              child: ListTile(
+                leading:
+                    Image(image: AssetImage(movie.movie[index].posterPath)),
+                title: Text(movie.movie[index].title),
+                trailing: PopupMenuButton(
+                  itemBuilder: (BuildContext context) {
+                    return buttonChoice.map((r) {
+                      return PopupMenuItem(
+                        value: r,
+                        child: Text(r),
+                        onTap: () {
+                          if (r == "Update") {
+                          } else if (r == "Hapus") {}
+                        },
+                      );
+                    }).toList();
+                  },
+                ),
               ),
             );
           }),
+    );
+  }
+
+  Widget addItem() {
+    return Container(
+      child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(label: Text("Title")),
+          ),
+          TextField(
+            decoration: InputDecoration(label: Text("Gambar")),
+          ),
+          ElevatedButton(onPressed: () {}, child: Text("Simapn"))
+        ],
+      ),
     );
   }
 }
